a2882a2f7544eaaf6321a1b80a57a81a3350acd3
codingcat08 <mehtaharshvardhansingh@gmail.com>
2023-12-23 16:07:56 +0530

    added phone number

--- src/App.js
+++ src/App.js
@@ -1,174 +1,9 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import "react-phone-number-input/style.css";
-// import PhoneInput from "react-phone-number-input";
-// import { Form, Button } from "react-bootstrap";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import OtpImg from "./Lock.png";
-// import InputField from "./InputField";
-// import Message from "./LeftBanner";
-// import "./register.css";
-// import "./App.css" ;
-// import loan from "./options.js"
-// import Dropdown from 'react-bootstrap/Dropdown';
-// const Register = () => {
-//   //const navigate = useNavigate();
-//   const [phno, setPhno] = useState("");
-//   const [isDisable, setIsDisable] = useState(false);
-//   const [flag, setFlag] = useState(false);
-//   const [otp, setOtp] = useState("");
-//   const [inputValue, setInputValue] = useState({
-//     name: "",
-//     email: "",
-//   });
-
-//   const { name, email } = inputValue;
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setInputValue((prev) => ({
-//       ...prev,
-//       [name]: value,
-//     }));
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     toast.success("Successful", {
-//       position: "top-center",
-//     });
-//     setIsDisable(true);
-//     setFlag(true);
-//   };
-
-//   return (
-//     <div className="registerWrapper">
-//       <div className="messageLogin">
-//         <Message />
-//       </div>
-//       <div className="userRegisterForm" style={{ display: !flag ? "flex" : "none" }}>
-//         <div className="midWrapperRegister">
-//           <div className="registerMessage">
-//             Sign Up to One
-//             <span className="changeCol font40 registerMessage">NDF</span>.in
-//           </div>
-//           <div className="welcomeMessage">
-//             Please enter your details to Sign Up.
-//           </div>
-//           <form onSubmit={handleSubmit} id="registerForm">
-//             <div className="nameWrap">
-//               <InputField
-//                 type="text"
-//                 value={name}
-//                 placeholder="Full Name"
-//                 name="name"
-//                 onChange={handleChange}
-//               />
-//             </div>
-//             <div className="phonenumWrap">
-//               <PhoneInput
-//                 defaultCountry="IN"
-//                 value={phno}
-//                 onChange={setPhno}
-//                 placeholder="Enter Phone Number"
-//                 required
-//               />
-//             </div>
-//             <div className="emailWrap">
-//               <InputField
-//                 type="text"
-//                 value={email}
-//                 placeholder="Email"
-//                 name="email"
-//                 onChange={handleChange}
-//               />
-//             </div>
-//             <div className="emailWrap">
-//               <InputField
-//                 type="text"
-//                 value={email}
-//                 placeholder="PAN "
-//                 name="email"
-//                 onChange={handleChange}
-//               />
-//             </div>
-//             <Dropdown
-//               name = "type"
-//               value = {loan} 
-//               options={loan}
-//               placeHolder = "Types of loan required"
-//               onChange= {handleChange}
-//               >
-//             </Dropdown>  
-//             <div className="emailWrap">
-//               <InputField
-//                 type="text"
-//                 value={email}
-//                 placeholder="Loan amount in INR"
-//                 name="email"
-//                 onChange={handleChange}
-//               />
-//             </div>
-
-//             <Button
-//               type="submit"
-//               variant="primary"
-//               className="yellowBtn"
-//               form="registerForm"
-//               disabled={isDisable}
-//             >
-//               Next
-//             </Button>
-//           </form>
-//           <div className="termsConditions">
-//             By signing up, you are agreeing to our
-//             <span className="termsConditions" style={{ fontWeight: "700" }}>
-//               Terms And Conditions.
-//             </span>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="verifyOTP" style={{ display: flag ? "flex" : "none" }}>
-//         <div className="midWrapperRegister">
-//           <div className="OtpImgCon">
-//             <img src={OtpImg} alt="/" className="OtpImg" />
-//           </div>
-//           <div className="otpMessage">OTP Verification</div>
-//           <div className="OtpSent">
-//             We have sent you a 6-digit OTP Verification code to{" "}
-//             <span className="OtpSent" style={{ fontWeight: "700" }}>
-//               {phno}
-//             </span>
-//           </div>
-//           <Form onSubmit={handleSubmit} style={{ width: "100%" }}>
-//             <Form.Group controlId="formBasicOtp">
-//               <Form.Control
-//                 type="otp"
-//                 placeholder="Enter OTP"
-//                 onChange={(e) => setOtp(e.target.value)}
-//                 className="form-control"
-//                 style={{ marginBottom: "2rem" }}
-//               />
-//             </Form.Group>
-//             <div className="otpSubmitButton">
-//               <Button type="submit" variant="primary" className="yellowBtn">
-//                 Verify
-//               </Button>
-//             </div>
-//           </Form>
-//         </div>
-//       </div>
-//       <ToastContainer />
-//     </div>
-//   );
-// };
-
-// export default Register;
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { ToastContainer, toast } from "react-toastify";
 import Select from "react-select";
+import PhoneInput from "react-phone-number-input";
+import "react-phone-number-input/style.css"; // Make sure to include the styles
 import OtpImg from "./Lock.png";
 import InputField from "./InputField";
 import Message from "./LeftBanner";
@@ -230,7 +65,13 @@ const Register = () => {
               />
             </div>
             <div className="phonenumWrap">
-              {/* Phone input field */}
+              <PhoneInput
+                defaultCountry="IN"
+                value={phno}
+                onChange={setPhno}
+                placeholder="Enter Phone Number"
+                required
+              />
             </div>
             <div className="emailWrap">
               <InputField
@@ -294,4 +135,3 @@ const Register = () => {
 };
 
 export default Register;
-
